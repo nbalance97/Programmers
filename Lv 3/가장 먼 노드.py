1e4a4483ae69a376aa6e686b5f585e7859a4ae3a
Byunghoon <76891875+nbalance97@users.noreply.github.com>
2021-07-12 10:15:06 +0900

    Update 가장 먼 노드.py

--- Lv 3/가장 먼 노드.py
+++ Lv 3/가장 먼 노드.py
@@ -25,3 +25,33 @@ def solution(n, edge):
     answer = distance.count(mdistance)
     
     return answer
+
+###################################### deque 활용 
+
+from collections import deque
+
+def solution(n, edge):
+    answer = 0
+    INT_MAX = 99999999999
+    queue = deque()
+    distance = [INT_MAX] * (n+1)
+    graph = [[] for _ in range(n+1)]
+    
+    for s, e in edge:
+        graph[s].append(e)
+        graph[e].append(s)
+    
+    distance[1] = 0
+    queue.append(1)
+    while queue:
+        p = queue.popleft()
+        for nx in graph[p]:
+            if distance[nx] > distance[p] + 1:
+                distance[nx] = distance[p] + 1
+                queue.append(nx)
+    
+    distance[0] = 0
+    mdistance = max(distance)
+    answer = distance.count(mdistance)
+    
+    return answer
