cbbfff27ed296bbe8677d7b8e868169d2d0f002f
Byunghoon <76891875+nbalance97@users.noreply.github.com>
2021-07-12 10:07:43 +0900

    Create 가장 먼 노드.py

---
+++ Lv 3/가장 먼 노드.py
@@ -0,0 +1,27 @@
+import heapq
+
+def solution(n, edge):
+    answer = 0
+    INT_MAX = 99999999999
+    heap = []
+    distance = [INT_MAX] * (n+1)
+    graph = [[] for _ in range(n+1)]
+    
+    for s, e in edge:
+        graph[s].append(e)
+        graph[e].append(s)
+    
+    distance[1] = 0
+    heapq.heappush(heap, 1) # vertex, 
+    while heap:
+        p = heapq.heappop(heap)
+        for nx in graph[p]:
+            if distance[nx] > distance[p] + 1:
+                distance[nx] = distance[p] + 1
+                heapq.heappush(heap, nx)
+    
+    distance[0] = 0
+    mdistance = max(distance)
+    answer = distance.count(mdistance)
+    
+    return answer
