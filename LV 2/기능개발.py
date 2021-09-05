ab5237fcb73a405a3861fb9c22662ba701f03e85
Byunghoon <76891875+nbalance97@users.noreply.github.com>
2021-09-05 11:31:22 +0900

    Update 기능개발.py
    2021-09-05 기능개발 다시 풀이

--- LV 2/기능개발.py
+++ LV 2/기능개발.py
@@ -1,24 +1,45 @@
+from collections import deque
 def solution(progresses, speeds):
     answer = []
-    progresses = progresses[::-1]
-    speeds = speeds[::-1]
-    
-    while True:
-        remainder = (100 - progresses[-1]) % speeds[-1]
-        days = (100 - progresses[-1]) // speeds[-1] 
-        if remainder != 0:
-            days = days + 1
+    progresses = deque(progresses)
+    speeds = deque(speeds)
+    while progresses:
+        # 맨 왼쪽 작업이 몇일 걸리는지 파악
+        days = (100 - progresses[0]) // (speeds[0])
+        days = days + 1 if (100 - progresses[0]) % speeds[0] != 0 else days
         
-        for i in range(len(progresses)):
-            progresses[i] = progresses[i] + speeds[i] * days
-
+        # 맨 왼쪽 작업이 종료되었을 때, 연쇄적으로 종료될수 있는 작업들 종료시키고 갯수 저장
         count = 0
-        while progresses and progresses[-1] >= 100:
+        while progresses and (100 - progresses[0]) <= speeds[0] * days:
             count += 1
-            progresses.pop()
-            speeds.pop()
+            progresses.popleft()
+            speeds.popleft()
         
         answer.append(count)
-        if not progresses:
-            break
     return answer
+
+
+# def solution(progresses, speeds):
+#     answer = []
+#     progresses = progresses[::-1]
+#     speeds = speeds[::-1]
+    
+#     while True:
+#         remainder = (100 - progresses[-1]) % speeds[-1]
+#         days = (100 - progresses[-1]) // speeds[-1] 
+#         if remainder != 0:
+#             days = days + 1
+        
+#         for i in range(len(progresses)):
+#             progresses[i] = progresses[i] + speeds[i] * days
+
+#         count = 0
+#         while progresses and progresses[-1] >= 100:
+#             count += 1
+#             progresses.pop()
+#             speeds.pop()
+        
+#         answer.append(count)
+#         if not progresses:
+#             break
+#     return answer
