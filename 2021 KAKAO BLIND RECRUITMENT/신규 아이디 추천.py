35c6fd924a6a26960b249568618f6b4fe3f153b6
Byunghoon <76891875+nbalance97@users.noreply.github.com>
2021-09-18 10:37:06 +0900

    Update 신규 아이디 추천.py
    2021-09-18 Refactoring

--- 2021 KAKAO BLIND RECRUITMENT/신규 아이디 추천.py
+++ 2021 KAKAO BLIND RECRUITMENT/신규 아이디 추천.py
@@ -1,3 +1,45 @@
+import re
+
+def solution(new_id):
+    answer = ''
+    # 1단계
+    new_id = new_id.lower()
+    
+    # 2단계
+    temp = re.findall(r"[a-z|0-9|\-|_|.]", new_id)
+    new_id = "".join(temp)
+    
+    # 3단계
+    temp = " "
+    for p in new_id:
+        if p == ".":
+            if temp[-1] != ".":
+                temp = temp + "."
+        else:
+            temp = temp + p
+    new_id = temp[1:]
+    
+    # 4단계   
+    new_id = new_id.strip('.')
+    
+    # 5단계
+    if len(new_id) == 0:
+        new_id = "a"
+    
+    # 6단계
+    if len(new_id) >= 16:
+        new_id = new_id[:15]
+        new_id = new_id.rstrip('.')
+    
+    # 7단계
+    if len(new_id) <= 2:
+        new_id = new_id + new_id[-1] * (3 - len(new_id))
+
+    answer = new_id
+    return answer
+
+# ----------------------------------------------------------------
+
 def solution(new_id):
     import re
     answer = ''
