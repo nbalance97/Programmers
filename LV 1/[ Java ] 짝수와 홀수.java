eba2a10f49798d69537b6d59d5faa75afd793117
Byunghoon <76891875+nbalance97@users.noreply.github.com>
2022-01-24 15:08:10 +0900

    Update [ Java ] 짝수와 홀수.java

--- LV 1/[ Java ] 짝수와 홀수.java
+++ LV 1/[ Java ] 짝수와 홀수.java
@@ -1,5 +1,8 @@
 class Solution {
     public String solution(int num) {
+        if (num < 0) {
+            num = -num;
+        }
         String answer = num % 2 == 1 ? "Odd" : "Even";
         return answer;
     }
