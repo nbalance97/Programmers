15c9fd609b91a281a6725299835b73ae75a34763
Byunghoon <76891875+nbalance97@users.noreply.github.com>
2022-02-28 09:30:20 +0900

    Create 점프와 순간 이동.java

---
+++ LV 2/점프와 순간 이동.java
@@ -0,0 +1,17 @@
+
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+                continue;
+            }
+            answer += 1;
+            n -= 1;
+        }
+
+        return answer;
+    }
+}
